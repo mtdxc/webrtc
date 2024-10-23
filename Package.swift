0bf636acb62c3c5c528e682e65203b4c3559001a
cqm <cqm@97kid.com>
2024-10-23 09:03:44 +0800

    update 124.0.2

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "WebRTC",
-            url: "https://github.com/jitsi/webrtc/releases/download/v124.0.1/WebRTC.xcframework.zip",
-            checksum: "d2548ea930ee135ea8269041c34031f388014d2b2c8aa5f7a7cfb892b603c31c"
+            url: "https://github.com/mtdxc/webrtc/releases/download/v124.0.2/WebRTC.xcframework.zip",
+            checksum: "0cfa625fc4d8ddb833c8f0bbc5ba83e162443fa01780df0e8d2973724f3391bd"
         ),
     ]
 )

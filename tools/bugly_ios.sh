45e7bda7e018285df77ee18daa208d83a7b2eeb9
cqm <cqm@97kid.com>
2025-08-19 15:20:54 +0800

    update script

---
+++ tools/bugly_ios.sh
@@ -0,0 +1,1 @@
+java -jar buglyqq-upload-symbol.jar -appid 05b3901e7a -appkey 4a8feb12-0872-43d5-890c-d79e334ebb97 -platform IOS -inputSymbol build_webrtc/webrtc/ios/src/out/Release-ios-device-arm64/WebRTC.dSYM
